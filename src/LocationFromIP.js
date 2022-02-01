5fe32d74f2a00ba9218b65de4cfce6e0b33675be
GigiFitiany <gfitiany@gmail.com>
2022-02-01 12:03:08 -0500

    lab3

--- src/LocationFromIP.js
+++ src/LocationFromIP.js
@@ -1,6 +1,8 @@
 // dependencies / things imported
 import { LitElement, html, css } from 'lit';
 import { UserIP } from './UserIP.js';
+import '@lrnwebcomponents/wikipedia-query/wikipedia-query.js';
+//important the wikipedia-query from nowhere or a "bare importt"
 
 export class LocationFromIP extends LitElement {
   static get tag() {
@@ -11,8 +13,10 @@ export class LocationFromIP extends LitElement {
     super();
     this.UserIpInstance = new UserIP();
     this.locationEndpoint = 'https://freegeoip.app/json/';
-    this.long = 10.305385;
-    this.lat = 77.923029;
+    this.long = null;
+    this.lat = null;
+    this.country = null;
+    this. city = null;
   }
 
   static get properties() {
@@ -38,6 +42,10 @@ export class LocationFromIP extends LitElement {
       })
       .then(data => {
         console.log(data);
+        this.long = data.longitude;
+        this.lat = data.latitude;
+        this.country = data.country;
+        this.city = data.city;
         return data;
       });
   }
@@ -59,8 +67,16 @@ export class LocationFromIP extends LitElement {
   render() {
     // this function runs every time a properties() declared variable changes
     // this means you can make new variables and then bind them this way if you like
+    console.log(`lat: ${this.lat} long ${this.long}`)
     const url = `https://maps.google.com/maps?q=${this.long},${this.lat}&t=&z=15&ie=UTF8&iwloc=&output=embed`;
-    return html`<iframe title="Where you are" src="${url}"></iframe> `;
+    return html`<iframe title="Where you are" src="${url}"></iframe> 
+    <a href="https://www.google.com/maps/@40.804,77.910,14z"><p style="text-align:left">Location on Google Maps</a></p>;
+    <br>
+      <script>window.__appCDN="https://cdn.webcomponents.psu.edu/cdn/";</script>
+      <script src="https://cdn.webcomponents.psu.edu/cdn/build.js"></script>
+      <wikipedia-query search="${this.city}"></wikipedia-query>
+      <wikipedia-query search="${this.region}"></wikipedia-query>
+      <wikipedia-query search="${this.city}, ${this.region}"></wikipedia-query> `;
   }
 }
 
