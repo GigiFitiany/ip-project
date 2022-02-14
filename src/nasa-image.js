a38949b3795947d0a68a91ed4113ffdd314236ec
abbeylyle4 <59837979+abbeylyle4@users.noreply.github.com>
2022-02-13 23:22:41 -0500

    Update nasa-image.js

--- src/nasa-image.js
+++ src/nasa-image.js
@@ -1,117 +1,120 @@
 import { LitElement, html, css } from 'lit';
 import '@lrnwebcomponents/accent-card';
-
-class NasaImage extends LitElement {
-  constructor() {
-    super();
-    this.NasaImages = [];
-    this.term = '';
-    this.startYear = 2000;
-    this.endYear = 2022;
-    this.listView = false;
-  }
-
-  static get properties() {
-    return {
-      term: { type: String, reflect: true },
-      NasaImages: { type: Array },
-      startYear: { type: Number },
-      endYear: { type: Number },
-      listView: { type: Boolean, reflect: true, attribute: 'list-view'},
-    };
-  }
-
-  updated(changedProperties) {
-    changedProperties.forEach((oldValue, propName) => {
-      if (propName === 'term' && this[propName]) {
-        this.getNasaData();
-      } else if (propName === 'NasaImages') {
-        this.render();
-      } else if (propName === 'NasaImages') {
-        this.dispatchEvent(
-          new CustomEvent('results-changed', {
-            detail: {
-              value: this.NasaImages,
-            },
-          })
-        );
-      }
-    });
-  }
-
-  updateTerm(value) {
-    this.term = value;
-    this.getNasaData();
-  }
-
-  async getNasaData() {
-    // let term = document.querySelector("term").value;
-    // document.querySelector("term").term = document.querySelector("term").value;
-    return fetch(
-      `https://images-api.nasa.gov/search?media_type=image&q=${this.term}`
-      // adding in the search term to the end of the url for updated search
-    )
-      .then(resp => {
-        if (resp.ok) {
-          return resp.json();
-        }
-        return false;
-      })
-      .then(data => {
-        // console.log(data);
-        this.NasaImages = [];
-
-        // grabbed from coursedates.js
-        data.collection.items.forEach(element => {
-          // not sure why we need collections
-          if (element.links[0].href !== undefined) {
-            const simplifiedInfo = {
-              imagesrc: element.links[0].href,
-              title: element.data[0].title,
-              description: element.data[0].description,
-            };
-            // console.log(simplifiedInfo);
-            this.NasaImages.push(simplifiedInfo);
-          }
-        });
-        return data;
-      });
-  }
-
-  static get styles() {
-    return css`
-      :host {
-        display: block;
-        border: 2px solid black;
-        min-height: 100px;
-      }
-      date-card {
-        display: inline-flex;
-      }
-      :host([view='list']) ul {
-        margin: 20px;
-      }
-    `;
-  }
-
-  render() {
-    return html`${this.listView === true
-    ? html`
-    <ul>
-        ${this.NasaImages.map(
-            item => html`
-        <><li>${item.title}</li><li>${item.imagesrc}</li><li>${item.description}</li></>
-        `
-        )}
-    </ul>
-    `
-    : html` ${this.NasaImages.map(
-        item => html`
-        <accent-card image-src="${item.imagesrc}">
-            <div slot="heading">${item.title}</div>
-            <div slot="content">${item.description}</div>
-        </accent-card>`
-    )}`}`;
-    }
+ 
+class NasaSearch extends LitElement {
+ constructor() {
+   super();
+   this.NasaImages = [];
+   this.term = '';
+   this.page = 1;
+   this.startYear = 2000;
+   this.endYear = 2022;
+   this.listView = false;
+ }
+ 
+ static get properties() {
+   return {
+     term: { type: String, reflect: true },
+     page: { type: Number, reflect: true },
+     NasaImages: {
+       type: Array,
+     },
+     startYear: { type: Number },
+     endYear: { type: Number },
+     listView: { type: Boolean, reflect: true, attribute: 'list-view' },
+     secondary_creator: { type: String, reflect: true },
+   };
+ }
+ 
+ updated(changedProperties) {
+   changedProperties.forEach((oldValue, propName) => {
+     if (propName === 'term' && this[propName]) {
+       this.getNasaData();
+     } else if (propName === 'NasaImages') {
+       this.render();
+     } else if (propName === 'NasaImages') {
+       this.dispatchEvent(
+         new CustomEvent('results-changed', {
+           detail: {
+             value: this.NasaImages,
+           },
+         })
+       );
+     }
+   });
+ }
+ 
+ updateTerm(value) {
+   this.term = value;
+   this.getNasaData();
+ }
+ 
+ async getNasaData() {
+   return fetch(
+     ` https://images-api.nasa.gov/search?q=${this.term}&page=${this.page}&year_start=${this.startYear}&year_end=${this.endYear}`
+   )
+     .then(resp => {
+       if (resp.ok) {
+         return resp.json();
+       }
+       return false;
+     })
+     .then(data => {
+       this.NasaImages = [];
+ 
+       data.collection.items.forEach(element => {
+         if (element.links[0].href !== undefined) {
+           const simplifiedInfo = {
+             imagesrc: element.links[0].href,
+             title: element.data[0].title,
+             description: element.data[0].description,
+             secondary_creator: element.data[0].secondary_creator,
+           };
+           // console.log(simplifiedInfo);
+           this.NasaImages.push(simplifiedInfo);
+         }
+       });
+       return data;
+     });
+ }
+ 
+ static get styles() {
+   return css`
+     :host {
+       display: block;
+       border: 2px solid black;
+       min-height: 100px;
+     }
+     date-card {
+       display: inline-flex;
+     }
+     :host([view='list']) ul {
+       margin: 20px;
+     }
+   `;
+ }
+ 
+ render() {
+   return html`${this.listView === true
+     ? html`
+         <ul>
+           ${this.NasaImages.map(
+             item => html`
+               <li>${item.title}</li>
+               <li>${item.imagesrc}</li>
+               <li>${item.description}</li>
+               <li>${item.secondary_creator}</li>
+             `
+           )}
+         </ul>
+       `
+     : html` ${this.NasaImages.map(
+         item => html` <accent-card image-src="${item.imagesrc}">
+           <div slot="heading">${item.title}</div>
+           <div slot="content">${item.description}</div>
+           <div>${item.secondary_creator}</div>
+         </accent-card>`
+       )}`}`;
+ }
 }
-customElements.define('nasa-image-search', nasa-image);
+customElements.define('nasa-image-search', NasaSearch);
